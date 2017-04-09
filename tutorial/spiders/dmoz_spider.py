ad1f9445560ad6c3b9319993d88d8a7a73c26947
zhenghefei <zhenghf@putao.com>
2017-04-09 13:45:16 +0800

    init

---
+++ tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+from tutorial.items import DmozItem
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["xiachufang.com"]
+    start_urls = [
+        "http://www.xiachufang.com/recipe/102251716/"
+    ]
+
+    def parse(self, response):
+        pass
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
